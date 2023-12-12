66fdf597c188f2e1f13390b0f0a23b47e9a529f6
benjaminhlina <benjamin.hlina@gmail.com>
2023-12-12 13:52:16 -0500

    Added in loading in data

--- R/Clean data/clean_telemetry_data_lmb_np_tor.R
+++ R/Clean data/clean_telemetry_data_lmb_np_tor.R
@@ -7,5 +7,10 @@
   library(sf)
   library(tidyr)
 }
-
 # ---- Bring in non summarized lmb and smb data ----
+
+dat <- qread(here("data-raw",
+                  "raw-lmb-np.qs"))
+glimpse(dat)
+
+# ---- calculate daily metrics for accel ----
