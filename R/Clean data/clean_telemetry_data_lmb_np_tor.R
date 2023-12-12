6e96ff3782cbaf639f06643ff9446420800ac4ca
benjaminhlina <benjamin.hlina@gmail.com>
2023-12-12 14:43:22 -0500

    Add in calculation for DOY and start to grab just ACCEL data

--- R/Clean data/clean_telemetry_data_lmb_np_tor.R
+++ R/Clean data/clean_telemetry_data_lmb_np_tor.R
@@ -13,4 +13,21 @@ dat <- qread(here("data-raw",
                   "raw-lmb-np.qs"))
 glimpse(dat)
 
-# ---- calculate daily metrics for accel ----
+# ---- calculate day of year and month abbreviation ----
+# data.table is far more powerfull than dplyr
+dat[, c("doy", "month_abb") := list(
+  yday(detection_timestamp_EST),
+  month(detection_timestamp_EST,
+        label = TRUE, abbr = TRUE)
+)
+]
+
+
+# ---- filter out just accell data ----
+# first set key which is how we filter in data.table
+
+setkey(dat, sensorv.type)
+
+# create sybset of just acceleration data
+dat_accel <- dat[sensorv.type %in% c("Accel")]
+
