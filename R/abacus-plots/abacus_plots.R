f7229ab57f3fd5509e2986fcd0830bb6c663c7a4
benjaminhlina <benjamin.hlina@gmail.com>
2023-12-15 12:58:06 -0500

    Added abacus plot loop using purrr and removed fish tagging metadata

--- R/abacus-plots/abacus_plots.R
+++ R/abacus-plots/abacus_plots.R
@@ -2,8 +2,10 @@
 {
   library(data.table)
   library(dplyr)
+  library(ggplot2)
   library(here)
   library(lubridate)
+  library(purrr)
   library(qs)
   library(readr)
 }
@@ -14,26 +16,6 @@ dat <- qread(here("data-raw",
 glimpse(dat)
 
 
-fish_tag <- read_csv(here("data-raw",
-                          "Fish Data Master List_Nov2022.csv")) %>%
-  janitor::clean_names() %>%
-  mutate(
-    sn = as.character(sn)
-  )
-
-glimpse(fish_tag)
-
-fish_tag <- setDT(fish_tag)
-# ---- calculate day of year and month abbreviation ----
-# data.table is far more powerfull than dplyr
-dat[, c("doy", "month_abb") := list(
-  yday(detection_timestamp_EST),
-  month(detection_timestamp_EST,
-        label = TRUE, abbr = TRUE)
-)
-]
-
-
 # ---- filter out just accell data ----
 # first set key which is how we filter in data.table
 
@@ -41,3 +23,35 @@ setkey(dat, sensorv.type)
 
 # create sybset of just acceleration data
 dat_accel <- dat[sensorv.type %in% c("Accel")]
+
+dat_accel[, station_no := factor(station_no)]
+unique(dat_accel$station)
+glimpse(dat_accel)
+# ---- use mapp to loop through and export abacus plots -----
+dat_accel %>%
+  split(.$animal_id) %>%
+  map(~ ggsave(
+    filename = here("plots",
+                    "abacus-plots",
+                    paste0(unique(.$animal_id),'.png')),
+    height = 7,
+    width = 11,
+    plot =
+      ggplot(data = ., aes(x = detection_timestamp_EST, y = station)) +
+      geom_point(aes(fill = station), shape = 21, size = 3, alpha = 0.5) +
+      geom_line(aes(group = 1)) + # we can remove line if it's distracting
+      scale_fill_viridis_d(begin = 0.25, end = 0.75,
+                           option = "D", name = "Station") +
+      theme_bw(
+        base_size = 15
+      ) +
+      theme(panel.grid = element_blank(),
+            plot.title = element_text(hjust = 0.5)
+      ) +
+      labs(
+        title = paste("Fish ID:", unique(.$animal_id), sep = " "),
+        x = "Date",
+        y = "Station")
+  )
+  )
+
