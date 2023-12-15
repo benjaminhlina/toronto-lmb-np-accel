239e47adefa6b86910fb363306dd2171f2a0af79
benjaminhlina <benjamin.hlina@gmail.com>
2023-12-15 12:47:35 -0500

    Intial committ...this file though is incomplete as we need tagging metadata to convert ADC values to m/s2...do not run this file for now. it needs to be cleaned up

---
+++ R/clean-data/clean_telemetry_data_lmb_np_tor.R
@@ -0,0 +1,82 @@
+# ---- Load Packages ----
+{
+  library(data.table)
+  library(dplyr)
+  library(here)
+  library(lubridate)
+  library(qs)
+}
+# ---- Bring in non summarized lmb and smb data ----
+
+dat <- qread(here("data-raw",
+                  "raw-lmb-np.qs"))
+glimpse(dat)
+
+# ---- calculate day of year and month abbreviation ----
+# data.table is far more powerfull than dplyr
+dat[, c("doy", "month_abb") := list(
+  yday(detection_timestamp_EST),
+  month(detection_timestamp_EST,
+        label = TRUE, abbr = TRUE)
+)
+]
+
+
+# ---- filter out just accell data ----
+# first set key which is how we filter in data.table
+
+setkey(dat, sensorv.type)
+
+# create sybset of just acceleration data
+dat_accel <- dat[sensorv.type %in% c("Accel")]
+
+
+m <- dat_accel %>%
+  distinct(transmitter_id)
+
+
+openxlsx::write.xlsx(m, here("data-raw",
+                             "transmitter_ids_2014-2016_THP.xlsx"))
+# look at what years to confirm we have the right years
+dat_accel %>%
+  distinct(year)
+
+# look at what projects to confirm we have the right project
+dat_accel %>%
+  distinct(glatos_project_receiver)
+
+
+# look at accell data overall
+glimpse(dat_accel)
+
+
+
+
+
+
+# ---- need to convert acceleation data to  m/s2 ----
+
+
+
+# ---- create summary dataframe of doy of year with sem and sd ect
+
+accel_sum <- dat_accel[, .(
+  n = (.N), # number of dets heard per day
+  n_det = uniqueN(station_no), # unique # of receviers heard on in DOY
+  mean_accel = mean(Sensor.Val),
+  sd_accel = sd(Sensor.Val),
+  sem_sensor = sd(Sensor.Val) / sqrt((.N))
+),
+keyby =
+  .(spp, animal_id, sex, length, weight,
+    doy, month, month_abb, season, year)
+
+]
+
+
+# ----- export summary dataframe for GAMM analysis ----
+
+qsave(accel_sum, here("data-saved",
+                      "summary-accel-doy",
+                      "lmb_np_summary_accel_doy.qs"))
+
