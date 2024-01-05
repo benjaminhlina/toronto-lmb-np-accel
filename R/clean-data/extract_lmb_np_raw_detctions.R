15ab859133861caab1d9edfbe46bf0af7963ab60
benjamin.hlina <benjamin.hlina@gmail.com>
2024-01-05 12:00:22 -0500

    Intial commit of cleaning of new data

---
+++ R/clean-data/extract_lmb_np_raw_detctions.R
@@ -0,0 +1,34 @@
+# ---- Load Packages ----
+{
+  library(data.table)
+  library(here)
+  library(qs)
+}
+
+# --- bring in files ----
+
+det_loc <- fread(here("data-raw",
+                      "detection-data-mlp",
+                      "THFHA_detectionsWithLocs_20230926_140917.csv"))
+
+# ---- subset out largemouth and np ----
+accel <- det_loc[common_name_e %in% c("Largemouth Bass", "Northern Pike")]
+
+# --- remove big dataframe to clear up RAM ----
+rm(det_loc)
+gc()
+
+# ---- filter out accel data ----
+dplyr::glimpse(accel)
+unique(accel$tag_model)
+unique(accel$common_name_e)
+gc()
+accel_lmb_np <- accel[tag_model %in% "V13A-1x"]
+
+gc()
+# ---- view accel data and save ----
+dplyr::glimpse(accel_lmb_np)
+
+qsave(accel_lmb_np, here("data-raw",
+                         "detection-data",
+                         "raw-lmb-np-correct.qs"))
