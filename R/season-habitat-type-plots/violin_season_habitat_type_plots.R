e58a0fc00b81ee8b76cd9e6edad45ff5cfedd3c3
benjamin.hlina <benjamin.hlina@gmail.com>
2024-01-22 10:51:13 -0500

    Changes plots so season is on the x and not habitat type

--- R/season-habitat-type-plots/violin_season_habitat_type_plots.R
+++ R/season-habitat-type-plots/violin_season_habitat_type_plots.R
@@ -74,59 +74,77 @@ p <- ggplot(data = dat,
        y = expression(paste("Mean Acceleration (m ", s^-2, ")"))
   )
 
-# p
+p
 
 ggsave(filename = here("plots",
                        "violin",
                        "habitat_type_spp_violin.png"),
        width = 11, height = 8.5, plot = p)
+# ---- violin plot with season invovled ----
+glimpse(dat_sum_hab_season)
 
 p1 <- ggplot(data = dat,
-             aes(x = habitat_type, y = mean_accel,
-                 fill = season)) +
-  geom_violin() +
+             aes(x = season, y = mean_accel,
+                 fill = habitat_type)) +
+  geom_violin(width = 1,
+              position = position_dodge(width = 0.8)
+              ) +
+  geom_errorbar(data = dat_sum_hab_season,
+                aes(x = season , y = accel,
+                    ymax = accel + sem,
+                    ymin = accel - sem,
+                    group = habitat_type), width = 0.05,
+                position = position_dodge(width = 0.8)) +
+  geom_point(data = dat_sum_hab_season, aes(
+    x = season, y = accel,
+    group = habitat_type), size = 2,
+    position = position_dodge(width = 0.8)) +
   facet_wrap(. ~ common_name_e) +
-  scale_fill_viridis_d(end = 0.85, name = "Season",
+  scale_fill_viridis_d(end = 0.85, name = "Habitat Type",
                        alpha = 0.5) +
+  scale_y_continuous(breaks = seq(0, 3.5, 0.5)) +
+  coord_cartesian(ylim = c(0, 4)) +
   theme_bw(
     base_size = 15
   ) +
   theme(
     panel.grid = element_blank(),
     strip.background = element_blank(),
-    axis.text.x = element_text(angle = 45, hjust = 1),
-    legend.position = c(0.15, 0.82),
+    # axis.text.x = element_text(angle = 45, hjust = 1),
+    legend.position = c(0.09, 0.85),
     legend.box.background = element_blank(),
     legend.background = element_blank()
   ) +
-  labs(x = "Habitat Type",
+  labs(x = "Season",
        y = expression(paste("Mean Acceleration (m ", s^-2, ")"))
   )
 
 # p1
 
-ggsave(filename = here("plots",
-                       "violin",
-                       "habitat_type_season_spp_violin.png"),
-       width = 11, height = 8.5, plot = p1)
-
-p2 <- p +
-  theme(
-    axis.title.x = element_blank(),
-    axis.text.x = element_blank(),
-    axis.ticks = element_blank()
-  )
-
+# ggsave(filename = here("plots",
+#                        "violin",
+#                        "habitat_type_season_spp_violin.png"),
+#        width = 11, height = 8.5, plot = p1)
+#
+# p2 <- p +
+#   theme(
+#     axis.title.x = element_blank(),
+#     axis.text.x = element_blank(),
+#     axis.ticks = element_blank()
+#   )
+#
 p3 <- p1 +
   theme(
     strip.text = element_blank(),
   )
 
-p4 <- p2 / p3
+p4 <- p / p3 +
+  plot_annotation(tag_levels = "a", tag_suffix = ")")
+
 
 ggsave(filename = here("plots",
                        "violin",
                        "habitat_type_and_season_spp_violin.png"),
-       width = 12, height = 9.5, plot = p4)
+       width = 13, height = 14, plot = p4)
 
 
