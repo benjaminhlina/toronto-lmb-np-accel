ac5cb7e7e1cbb62051bca8779c6e093bfcfc76a3
benjaminhlina <benjamin.hlina@gmail.com>
2024-03-23 06:30:22 -0400

    added letters of signfance, facet_rep_wrap

--- R/season-habitat-type-plots/boxplot_season_habitat_type_publication_plots.R
+++ R/season-habitat-type-plots/boxplot_season_habitat_type_publication_plots.R
@@ -85,27 +85,28 @@ among_season_long$common_name_e
 unique(dat$common_name_e)
 
 # ---- letter prep ----
-
 sig_let_hab <-  tibble(
   common_name_e = among_hab_long$common_name_e,
   letter = among_hab_long$letters,
   x = rep(seq(1, 5, 1), 2),
-  y = c(
+  y =
+    c(
     1.17, 1.30, 1.72, 1.05, 1.29,
     0.37, 0.54, 0.42, 0.48, 0.42
 
   )
 )
 
+
 # ----- create boxplots ----
 p5 <- ggplot(data = dat,
              aes(x = habitat_type, y = mean_accel)) +
   geom_boxplot(width = 0.25, outlier.shape = NA) +
   geom_text(data = sig_let_hab, aes(x = x,
-                                y = y,
-                                label = letter),
+                                    y = y,
+                                    label = letter),
             size = 5) +
-  facet_wrap(. ~ common_name_e) +
+  lemon::facet_rep_wrap(. ~ common_name_e, repeat.tick.labels = TRUE) +
   theme_bw(
     base_size = 15
   ) +
@@ -117,6 +118,7 @@ p5 <- ggplot(data = dat,
   labs(x = "Habitat Type",
        y = expression(paste("Mean Acceleration (m ", s^-2, ")"))
   )
+
 
 
 
@@ -136,71 +138,54 @@ p5 <- ggplot(data = dat,
 # m[[4]]
 # quantile()
 max_ac <- dat %>%
-  group_by(season, habitat_type) %>%
+  group_by(season, habitat_type, common_name_e) %>%
   summarise(
     q1 = as.numeric(quantile(mean_accel, na.rm = TRUE, names = TRUE)[2]),
     q3 = as.numeric(quantile(mean_accel, na.rm = TRUE)[4]),
     iqr = q3 - q1,
-    whisk_high = q3 + (1.5 * iqr),
+    whisk_high = min(q3 + (1.5 * iqr), max(mean_accel), na.rm = TRUE),
     whisk_low = (1.5 * iqr) - q1
   ) %>%
   ungroup() %>%
-  arrange(season, habitat_type) %>%
-  filter(!(season %in% "Winter" &
+  arrange(common_name_e, season, habitat_type) %>%
+  filter(!(common_name_e %in% "Largemouth Bass" &
+           season %in% "Winter" &
              habitat_type %in% c("Exposed/Low SAV",
                                  "Mod/Dense SAV")))
 
-max_ac
+print(max_ac, n = 36)
 
+
+# top = min(q3 + 1.5*iqr, max(mpg)),
 dput(unique(dat$habitat_type))
 
-sig_let <- tibble(
-  common_name_e = within_season_long$common_name_e,
-  letter = within_season_long$letters,
-  x = c(
-    0.7, 0.85, 1, 1.15, 1.3,
-    1.75, 2, 2.25,
-    2.7, 2.85, 3, 3.15, 3.3,
-    3.7, 3.85, 4, 4.15, 4.3,
-    0.7, 0.85, 1, 1.15, 1.3,
-    1.7, 1.85, 2, 2.15, 2.3,
-    2.7, 2.85, 3, 3.15, 3.3,
-    3.7, 3.85, 4, 4.15, 4.3
-  ),
-  y = c(
-    0.75, 0.93, 1.1, 0.85, 1.05,
-    0.63, 0.4, 0.45,
-    1.33, 0.85, 1.15, 1.25, 1.1,
-    1.93, 1.73, 1.75, 1.73, 1.92,
-    0.4, 0.41, 0.38, 0.48, 0.37,
-    0.3, 0.47, 0.38, 0.65, 0.27,
-    0.5, 0.55, 0.5, 0.4, 0.61,
-    0.55, 0.53, 0.55, 0.47, 0.53
-  )
-)
 
-sig_stars <- tibble(
-  common_name_e = among_season_long$common_name_e,
-  stars = among_season_long$stars,
-  x = c(
-    0.7, 0.85, 1, 1.15, 1.3,
-    1.75, 2, 2.25,
-    2.7, 2.85, 3, 3.15, 3.3,
-    3.7, 3.85, 4, 4.15, 4.3,
-    0.7, 0.85, 1, 1.15, 1.3,
-    1.7, 1.85, 2, 2.15, 2.3,
-    2.7, 2.85, 3, 3.15, 3.3,
-    3.7, 3.85, 4, 4.15, 4.3
-  ),
-  y = c(0.75, 0.93, 1.1, 0.85, 1.05,
-        0.63, 0.4, 0.45,
-        1.33, 0.85, 1.15, 1.25, 1.1,
-        1.93, 1.73, 1.75, 1.73, 1.92,
-        0.4, 0.41, 0.38, 0.48, 0.37,
-        0.3, 0.47, 0.38, 0.65, 0.27,
-        0.5, 0.55, 0.5, 0.4, 0.61,
-        0.55, 0.53, 0.55, 0.47, 0.53)
-)
+# sig_stars <- tibble(
+#   common_name_e = among_season_long$common_name_e,
+#   stars = among_season_long$stars,
+#   x = c(
+#     0.7, 0.85, 1, 1.15, 1.3,
+#     1.75, 2, 2.25,
+#     2.7, 2.85, 3, 3.15, 3.3,
+#     3.7, 3.85, 4, 4.15, 4.3,
+#     0.7, 0.85, 1, 1.15, 1.3,
+#     1.7, 1.85, 2, 2.15, 2.3,
+#     2.7, 2.85, 3, 3.15, 3.3,
+#     3.7, 3.85, 4, 4.15, 4.3
+#   ),
+#   y =
+#     # max_ac$whisk_high
+#     c(
+#       0.75, 0.93, 1.1, 0.85, 1.05,
+#       0.63, 0.4, 0.45,
+#       1.33, 0.85, 1.15, 1.25, 1.1,
+#       1.93, 1.73, 1.75, 1.73, 1.92,
+#       0.4, 0.41, 0.38, 0.48, 0.37,
+#       0.3, 0.47, 0.38, 0.65, 0.27,
+#       0.5, 0.55, 0.5, 0.4, 0.61,
+#       0.55, 0.53, 0.55, 0.47, 0.53
+#     )
+#     )
 
 
 
@@ -212,16 +197,13 @@ p12 <- ggplot() +
   geom_boxplot(data = dat,
                aes(x = season, y = mean_accel,
                    fill = habitat_type), outlier.shape = NA) +
-  geom_text(data = sig_let, aes(x = x,
-                                y = y,
-                                label = letter),
-            size = 5) +
 
-  geom_text(data = sig_stars, aes(x = x,
-                                  y = y + 0.05,
-                                  label = stars),
-            size = 5) +
-  facet_wrap(. ~ common_name_e) +
+
+  # geom_text(data = sig_stars, aes(x = x,
+  #                                 y = y + 0.05,
+  #                                 label = stars),
+  #           size = 5) +
+  lemon::facet_rep_wrap(. ~ common_name_e, repeat.tick.labels = TRUE) +
   scale_fill_viridis_d(end = 0.85, name = "Habitat Type",
                        alpha = 0.5) +
   theme_bw(
@@ -239,7 +221,42 @@ p12 <- ggplot() +
        y = expression(paste("Mean Acceleration (m ", s^-2, ")"))
   )
 
-# p12
+
+ggstat <- ggplot_build(p12)$data
+
+sig_let <- tibble(
+  common_name_e = within_season_long$common_name_e,
+  letter = within_season_long$letters,
+  x = c(
+    0.7, 0.85, 1, 1.15, 1.3,
+    1.75, 2, 2.25,
+    2.7, 2.85, 3, 3.15, 3.3,
+    3.7, 3.85, 4, 4.15, 4.3,
+    0.7, 0.85, 1, 1.15, 1.3,
+    1.7, 1.85, 2, 2.15, 2.3,
+    2.7, 2.85, 3, 3.15, 3.3,
+    3.7, 3.85, 4, 4.15, 4.3
+  ),
+  y = ggstat[[1]]$ymax
+    # max_ac$whisk_high
+  #   c(
+  #   0.75, 0.93, 1.1, 0.85, 1.05,
+  #   0.63, 0.4, 0.45,
+  #   1.33, 0.85, 1.15, 1.25, 1.1,
+  #   1.93, 1.73, 1.75, 1.73, 1.92,
+  #   0.4, 0.41, 0.38, 0.48, 0.37,
+  #   0.3, 0.47, 0.38, 0.65, 0.27,
+  #   0.5, 0.55, 0.5, 0.4, 0.61,
+  #   0.55, 0.53, 0.55, 0.47, 0.53
+  # )
+)
+
+p12 <- p12 +
+  geom_text(data = sig_let, aes(x = x,
+                                y = y + 0.1,
+                                label = letter),
+            size = 5)
+p12
 # p12$data
 # glimpse(p12)
 # p12
@@ -256,11 +273,13 @@ p13 <- (p5 +
 ggsave(filename = here("plots",
                        "boxplot",
                        "Publication Plots",
-                       "hab_season_spp_boxplot_labelled_no_outlier.png"),
+                       paste("hab_season_spp_boxplot_labelled_no_outlier_",
+                             Sys.Date(), ".png", sep = "")),
        width = 14, height = 11, plot = p13)
 ggsave(filename = here("plots",
                        "boxplot",
                        "Publication Plots",
-                       "hab_season_spp_boxplot_labelled_no_outlier.pdf"),
+                       paste("hab_season_spp_boxplot_labelled_no_outlier_",
+                             Sys.Date(), ".pdf", sep = "")),
        width = 14, height = 11, plot = p13)
 
