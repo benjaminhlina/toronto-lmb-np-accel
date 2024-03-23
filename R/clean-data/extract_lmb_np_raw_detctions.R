1713efd2676589f3b7e8cfd028808e95b32a1278
benjaminhlina <benjamin.hlina@gmail.com>
2024-03-23 06:28:51 -0400

    added q save for raw-to-detect

--- R/clean-data/extract_lmb_np_raw_detctions.R
+++ R/clean-data/extract_lmb_np_raw_detctions.R
@@ -15,7 +15,7 @@ det_loc <- fread(here("data-raw",
 accel <- det_loc[common_name_e %in% c("Largemouth Bass", "Northern Pike")]
 
 # --- remove big dataframe to clear up RAM ----
-rm(det_loc)
+# rm(det_loc)
 gc()
 
 # ---- filter out accel data ----
@@ -32,3 +32,6 @@ dplyr::glimpse(accel_lmb_np)
 qsave(accel_lmb_np, here("data-raw",
                          "detection-data",
                          "raw-lmb-np-correct.qs"))
+qsave(det_loc, here("data-raw",
+                         "detection-data",
+                         "raw-to-det.qs"))
