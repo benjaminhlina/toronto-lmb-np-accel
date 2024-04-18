d456c0abfc370869d1d2917ce8e51a21ce2a4517
benjaminhlina <benjamin.hlina@gmail.com>
2024-04-18 14:53:10 -0400

    Intial commit of revised boxplot

---
+++ R/accel-glmm-plots/combined_plots_boxplot.R
@@ -0,0 +1,41 @@
+{
+  library(here)
+  library(ggplot2)
+  library(qs)
+  library(patchwork)
+}
+
+
+lmb <- qread(here("data-saved",
+                  "predicted-plots",
+                  "lmb_season_hab_dp_plot_boxplot.qs"))
+
+lmb
+
+np <- qread(here("data-saved",
+                 "predicted-plots",
+                 "np_season_hab_dp_plot_boxplot.qs"))
+
+
+p <- (lmb +
+        theme(
+          legend.position = "inside",
+          legend.position.inside = c(0.9, 0.87)
+        ) +
+        labs(x = "")) /
+  (np +
+     theme(
+       legend.position = "none"
+     ) +
+     labs(x = "Diel Period")) +
+  plot_annotation(tag_levels = "a", tag_suffix = ")")
+
+p
+
+
+ggsave(here("plots",
+            "boxplot",
+            "Publication Plots",
+            "combined",
+            "combined_plot_boxplot.png"),
+       height = 16, width = 12, plot = p)
